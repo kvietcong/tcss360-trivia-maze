e036a4adf53bfda6c00bf3306af78aede76ade1a
KV Le <32186663+kvietcong@users.noreply.github.com>
2021-05-14 22:45:15 -0700

    🎮 Add base for MVC and Singleton state architecture

--- trivia-maze/src/state/GameState.java
+++ trivia-maze/src/state/GameState.java
@@ -4,16 +4,35 @@ import maze.Maze;
 import maze.Room;
 import question.Question;
 
+import java.beans.PropertyChangeListener;
 import java.io.Serializable;
 import java.util.Map;
+import java.util.Set;
 
 public interface GameState extends Serializable {
+    enum GameEvent {
+        MOVE
+    }
+
     GameState getInstance();
 
-    void loadState(String info);
-    void saveState(String info);
+    void loadState(String loadInfo);
+    void loadState(Maze maze, Map<Room, Question> questions);
+    void saveState(String saveInfo);
     void initiateState();
 
     Maze getMaze();
+    Room getCurrentRoom();
+    void moveToRoom(Room newRoom);
+    Question getRoomQuestion(Room room);
+    Set<Room> getCurrentNeighbors();
     Map<Room, Question> getQuestions();
+
+    void addPropertyChangeListener(final PropertyChangeListener theListener);
+    void addPropertyChangeListener(final GameEvent event,
+                                   final PropertyChangeListener theListener);
+
+    void removePropertyChangeListener(final PropertyChangeListener theListener);
+    void removePropertyChangeListener(final GameEvent event,
+                                      final PropertyChangeListener theListener);
 }
--- trivia-maze/src/state/GameStateSimple.java
+++ trivia-maze/src/state/GameStateSimple.java
@@ -1,33 +1,76 @@
 package state;
 
-import java.util.HashMap;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.Map;
+import java.util.Set;
 
 import maze.Maze;
-import maze.MazeGraph;
 import maze.Room;
 import question.Question;
 
-public class GameStateSimple {
-    private static final GameStateSimple INSTANCE = new GameStateSimple();
+public class GameStateSimple implements GameState {
+    private static final GameState STATE = new GameStateSimple();
 
     private Maze maze;
     private Map<Room, Question> questions;
+    private Room currentRoom;
+    private PropertyChangeSupport propertyChangeSupport;
 
     private GameStateSimple() {
-        initiateState();
+        propertyChangeSupport = new PropertyChangeSupport(this);
     }
 
-    public static GameState getInstance() { return (GameState) INSTANCE; }
+    public GameState getInstance() { return STATE; }
 
-    public void loadState(String info) { }
+    public void loadState(Maze maze, Map<Room, Question> questions) {
+        this.maze = maze;
+        this.questions = questions;
+    }
 
-    public void saveState(String info) { }
+    public void loadState(String loadInfo) { }
 
-    // Preload or Generate a new Maze
-    public void initiateState() { }
+    // TODO: Saving
+    public void saveState(String saveInfo) { }
 
-    public Maze getMaze() { return INSTANCE.maze; }
+    // TODO: Preload or Generate a new Maze
+    public void initiateState() {
+        propertyChangeSupport = new PropertyChangeSupport(this);
+    }
 
-    public Map<Room, Question> getQuestions() { return INSTANCE.questions; }
+    public Maze getMaze() { return maze; }
+
+    public Map<Room, Question> getQuestions() { return questions; }
+
+    public Room getCurrentRoom() { return currentRoom; }
+
+    public Question getRoomQuestion(Room room) { return questions.getOrDefault(room, null); }
+
+    public void moveToRoom(Room newRoom) {
+        Room oldRoom = currentRoom;
+        currentRoom = newRoom;
+        propertyChangeSupport.firePropertyChange(GameEvent.MOVE.toString(), oldRoom, newRoom);
+    }
+
+    public Set<Room> getCurrentNeighbors() {
+        return maze.getNeighbors(currentRoom);
+    }
+
+    public void addPropertyChangeListener(final PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void addPropertyChangeListener(final GameEvent event,
+                                          final PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(event.toString(), listener);
+    }
+
+    public void removePropertyChangeListener(final PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(final GameEvent event,
+                                             final PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(event.toString(), listener);
+    }
 }

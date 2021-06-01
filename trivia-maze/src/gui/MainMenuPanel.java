e80ef93d17aa697270a2e6c086a3167d4dd9aa54
Merit Mamo <meritayele@gmail.com>
2021-05-31 23:23:02 -0700

    added load state

--- trivia-maze/src/gui/MainMenuPanel.java
+++ trivia-maze/src/gui/MainMenuPanel.java
@@ -1,12 +1,18 @@
 package gui;
 
+import state.GameState;
+import state.GameStateSimple;
+
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.util.function.Consumer;
 
 public class MainMenuPanel extends JPanel {
 
     private final GridBagConstraints gbc;
+
+    private final GameState state =  GameStateSimple.getInstance();
     /**
      * Initialize the main menu panel
      * */
@@ -21,7 +27,7 @@ public class MainMenuPanel extends JPanel {
         add(newGameButton, gbc);
 
         //options button
-        JButton options = new JButton("Options");
+        JButton options = new JButton("Load Game");
         gbc.gridy++;
         add(options,gbc);
 
@@ -34,7 +40,7 @@ public class MainMenuPanel extends JPanel {
         newGameButton.addActionListener(action -> newGame.run());
         options.addActionListener(action -> {});
         exitButton.addActionListener(action -> exit.run());
-        options.addActionListener(action -> {});
+        options.addActionListener(action -> loadGame());
         exitButton.addActionListener(action -> exit.run());
     }
 
@@ -45,6 +51,22 @@ public class MainMenuPanel extends JPanel {
         gbc.gridy = 0;
         gbc.ipadx = 50;
         gbc.ipady = 20;
+    }
+
+    private void loadGame(){
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter extensionfilter = new FileNameExtensionFilter("Maze file(.maze)", "maze");
+        int selectedOption;
+        fileChooser.setDialogTitle("Open a file");
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.setFileFilter(extensionfilter);
+        selectedOption = fileChooser.showOpenDialog(this);
+
+        if (selectedOption == JFileChooser.APPROVE_OPTION) {
+            java.io.File file = fileChooser.getSelectedFile();
+            state.loadState(file.getPath());
+        }
     }
 }
 
